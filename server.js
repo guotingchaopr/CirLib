f7ca0ebbd8bde821aba473026fa22a626dea42ad
guotingchaopr <guotingchaopr@gmail.com>
2014-05-13 09:44:07 +0800

    init gh-pages

--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var express = require('express');
-var app = express();
-app.use("/", express.static(__dirname + '/static'));
-// 创建服务端
-http.createServer(app).listen('8080', function () {
-	console.log('启动服务器完成');
-});
