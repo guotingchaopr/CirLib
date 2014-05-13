31346466fdfc2f334512c54e0d13066e568860f1
guotingchaopr <guotingchaopr@gmail.com>
2014-05-13 10:25:00 +0800

    修改路径绑定git master分支

--- js/jscolor.js
+++ js/jscolor.js
@@ -13,7 +13,7 @@
 var jscolor = {
 
 
-	dir : 'img/', // location of jscolor directory (leave empty to autodetect)
+	dir : 'https://raw.githubusercontent.com/guotingchaopr/CirLib/master/static/img/', // location of jscolor directory (leave empty to autodetect)
 	bindClass : 'color', // class name
 	binding : true, // automatic binding via <input class="...">
 	preloading : true, // use image preloading?
