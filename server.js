15911ad95a146b1b3ae4c54a02033ccc73581f18
guotingchaopr <guotingchaopr@gmail.com>
2014-05-17 14:33:32 +0800

    ..

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+app.use(express.logger());
 app.use("/", express.static(__dirname + '/static'));
+app.get("/",function(req,res,next){
+     console.log(" req_url : " + req.url);
+     res.redirect('/index.htm');
+});
 // 创建服务端
 http.createServer(app).listen('8080', function () {
 	console.log('启动服务器完成');
